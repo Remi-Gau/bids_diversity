d3e2b944466cc692b2beab34568ff395b2dc2ef1
Remi Gau <remi_gau@hotmail.com>
2023-06-05 11:18:30 -0400

    start listing authors

--- list_openneuro_authors.py
+++ list_openneuro_authors.py
@@ -8,6 +8,33 @@ from rich import print
 
 openneuro_super_ds = Path("/home/remi/datalad/datasets.datalad.org/openneuro")
 
+AUTHORS_TO_IGNORE = ["TODO:"]
+
+
+def list_authors(ds_desc: dict):
+    """List authors from dataset description."""
+    authors = ds_desc.get("Authors")
+
+    if authors is None:
+        return None
+
+    sanitized_authors = []
+
+    if len(authors) == 1:
+        authors = authors[0]
+    if isinstance(authors, str):
+        authors = authors.split("., ")
+
+    for author_ in authors:
+        if author_ in AUTHORS_TO_IGNORE:
+            continue
+        author_ = re.sub(r"\(.*\)", "", author_)
+        author_ = author_.replace(",", "")
+        author_ = author_.replace(".", "")
+        sanitized_authors.append(author_.lower())
+
+    return sanitized_authors
+
 
 def main():
     """Get demographic data from openneuro datasets."""
@@ -18,6 +45,7 @@ def main():
     ]
 
     authors = []
+    ethics = []
 
     for ds in datasets:
         if not (ds / "dataset_description.json").exists():
@@ -26,26 +54,22 @@ def main():
         with open(ds / "dataset_description.json") as f:
             ds_desc = json.load(f)
 
-        new_authors = ds_desc.get("Authors")
+        new_authors = list_authors(ds_desc)
+        if new_authors is not None:
+            authors.extend(new_authors)
 
-        if new_authors is None:
-            continue
-
-        # sanitize authors
-        if len(new_authors) == 1:
-            new_authors = new_authors[0]
-        if isinstance(new_authors, str):
-            new_authors = new_authors.split("., ")
-
-        for author_ in new_authors:
-            if author_ in ["TODO:"]:
-                continue
-            author_ = re.sub(r"\(.*\)", "", author_)
-            author_ = author_.replace(",", "")
-            author_ = author_.replace(".", "")
-            authors.append(author_.lower())
+        if ds_desc.get("EthicsApprovals") not in (None, ""):
+            ethics.extend(ds_desc.get("EthicsApprovals"))
 
     authors = sorted(set(authors))
-    print(authors)
 
-    print(len(authors))
+    print(authors)
+    print(f"nb authors found: {len(authors)}")
+
+    ethics = sorted(set(ethics))
+    print(ethics)
+    print(len(ethics))
+
+
+if __name__ == "__main__":
+    main()
